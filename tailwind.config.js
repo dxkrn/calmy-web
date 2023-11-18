707402e859018853271f806ca89b2c92b0b39478
dxkrnn <dxkrnn@gmail.com>
2023-11-18 18:40:36 +0700

    feat(pages): create home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Poppins", "Arial", "sans-serif"],
+    },
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins"],
+      },
+      colors: {
+        primary: "#4349FF",
+        primaryDark: "#2A2FBA",
+        primaryLight: "#A4A7FF",
+        black: "#0A0C1C",
+        white: "#ffffff",
+        grey: "#7C7F9A",
+        bgDark: "#0000000D",
+      },
+      backgroundImage: {
+        "main-bg": "url('/src/images/background.svg')",
+      },
+    },
   },
   plugins: [],
 };
